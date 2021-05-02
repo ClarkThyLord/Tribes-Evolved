f0ec459a46938e2adaac7971ce6b44f2cf984135
ClarkThyLord <ccpotential@gmail.com>
2021-05-02 10:35:25 -0700

    Basic entity movement
    Clark was here :V

--- src/entity/entity.gd
+++ src/entity/entity.gd
@@ -7,11 +7,15 @@ extends Area2D
 ## Exported Variables
 export(float, 0.0, 100.0) var energy := 25.0 setget set_energy
 
+export(float, 0.0, 100.0) var speed := 1.0 setget set_speed
+
 export(float, 16.0, 100.0) var vision := 32.0 setget set_vision
 
 export var variance := Color.white setget set_variance
 
 export(int, 5, 64) var potential := 5 setget set_potential
+
+export var parent_world : NodePath
 
 
 
@@ -22,10 +26,14 @@ var _selected := false
 
 var _image : Image = null
 
+var _target := Vector2.INF
+
 
 
 ## OnReady Variables
 onready var collision_shape : CollisionShape2D = get_node("CollisionShape2D")
+
+onready var view : Area2D = get_node("View")
 
 onready var view_shape : CollisionShape2D = get_node("View/CollisionShape2D")
 
@@ -46,6 +54,27 @@ func _ready() -> void:
 		(view_shape.shape as CircleShape2D).radius = vision
 
 
+func _process(delta : float) -> void:
+	var world = get_node_or_null(parent_world)
+	if is_instance_valid(world) \
+			and is_instance_valid(view) \
+			and _target == Vector2.INF:
+		var areas := view.get_overlapping_areas()
+		for area in areas:
+			area = area as Area2D
+			if area.is_in_group("foods"):
+				_target = area.position
+	
+	if _target == Vector2.INF:
+		_target = world.random_world_point()
+	
+	var distance := position.distance_to(_target)
+	if distance > 8:
+		translate(position.direction_to(_target) * speed)
+	else:
+		_target = Vector2.INF
+
+
 func _draw() -> void:
 	if _hovered or _selected:
 		var color : Color
@@ -64,6 +93,10 @@ func _draw() -> void:
 ## Public Methods
 func set_energy(value : float) -> void:
 	energy = value
+
+
+func set_speed(value : float) -> void:
+	speed = value
 
 
 func set_vision(value : float) -> void:
