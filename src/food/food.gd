dec5afb3af372bb9bb1790d73aa5036aa9cf497c
ClarkThyLord <ccpotential@gmail.com>
2021-05-02 07:03:30 -0700

    Create food
    Clark was here :V

---
+++ src/food/food.gd
@@ -0,0 +1,60 @@
+tool
+extends Area2D
+## Food
+
+
+
+## Exported Variables
+export(float, 1.0, 100.0) var nutrition := 0.0 setget set_nutrition
+
+export var nutrition_type := Color.white setget set_nutrition_type
+
+
+
+## OnReady Variables
+onready var collision_shape : CollisionShape2D = get_node("CollisionShape2D")
+
+
+
+## Built-In Virtual Methods
+func _ready() -> void:
+	if is_instance_valid(collision_shape) \
+			and is_instance_valid(collision_shape.shape):
+				collision_shape.shape = collision_shape.shape.duplicate()
+	
+	update()
+
+
+func _draw() -> void:
+	draw_circle(
+		Vector2.ZERO,
+		_get_pixel_size(),
+		nutrition_type
+	)
+	if is_instance_valid(collision_shape) \
+			and is_instance_valid(collision_shape.shape):
+		(collision_shape.shape as CircleShape2D).radius = _get_pixel_size()
+
+
+
+## Public Methods
+func set_nutrition(value : float) -> void:
+	nutrition = value
+	update()
+
+
+func set_nutrition_type(color : Color) -> void:
+	nutrition_type = color
+	update()
+
+
+func randomize() -> void:
+	nutrition = randi() % 101
+	nutrition_type = Color(randi())
+	update()
+
+
+
+## Private Methods
+func _get_pixel_size() -> float:
+	return 2.0 + ((nutrition / 100.0) * 6.0)
