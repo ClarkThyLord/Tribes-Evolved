3647f5fce843e36a69b98890f14f6f0dd7324ab7
ClarkThyLord <ccpotential@gmail.com>
2021-05-02 08:55:48 -0700

    Working on entity
    Clark was here :V

--- src/entity/entity.gd
+++ src/entity/entity.gd
@@ -4,6 +4,17 @@ extends Area2D
 
 
 
+## Exported Variables
+export(float, 0.0, 100.0) var energy := 25.0
+
+export(float, 16.0, 100.0) var vision := 32.0
+
+export var variance := Color.white
+
+export(int, 5, 64) var potential := 5 setget set_potential
+
+
+
 ## Private Variables
 var _hovered := false
 
@@ -14,34 +25,27 @@ var _image := Image.new()
 
 
 ## OnReady Variables
+onready var collision_shape : CollisionShape2D = get_node("CollisionShape2D")
+
+onready var view_shape : CollisionShape2D = get_node("View/CollisionShape2D")
+
 onready var sprite : Sprite = get_node("Sprite")
 
 
 
 ## Built-In Virtual Methods
 func _ready() -> void:
+	evolve()
 	
-	var size = 5
-	var axis = (size - 1) / 2
-	var color = Color.red
-	_image.create(size, size, false, Image.FORMAT_RGB8)
+	if is_instance_valid(collision_shape) \
+			and is_instance_valid(collision_shape.shape):
+		collision_shape.shape = collision_shape.shape.duplicate()
+		(collision_shape.shape as RectangleShape2D).extents = Vector2.ONE * potential
 	
-	_image.lock()
-	for x in range(axis + 1):
-		for y in range(size):
-			if (randi() % 10) % 2 == 0:
-				_image.set_pixel(x, y, color)
-				if not x == axis:
-					_image.set_pixel(size - x - 1, y, color)
-	_image.unlock()
-	
-	var texture = ImageTexture.new()
-	texture.create_from_image(_image, 0)
-	sprite.texture = texture
-
-
-func _process(delta : float) -> void:
-	update()
+	if is_instance_valid(view_shape) \
+			and is_instance_valid(view_shape.shape):
+		view_shape.shape = view_shape.shape.duplicate()
+		(view_shape.shape as CircleShape2D).radius = vision
 
 
 func _draw() -> void:
@@ -59,16 +63,51 @@ func _draw() -> void:
 
 
 
+## Public Methods
+func set_potential(value : int) -> void:
+	potential = value
+	# FORCE ODD ?
+#	potential = value + ((value + 1) % 2)
+#	property_list_changed_notify()
+
+
+func evolve() -> void:
+	var axis = (potential - 1) / 2
+	var color = Color.red
+	_image.create(potential, potential, false, Image.FORMAT_RGB8)
+	
+	_image.lock()
+	for x in range(axis + 1):
+		for y in range(potential):
+			if (randi() % 10) % 2 == 0:
+				_image.set_pixel(x, y, color)
+				if not x == axis:
+					_image.set_pixel(potential - x - 1, y, color)
+	_image.unlock()
+	
+	var texture = ImageTexture.new()
+	texture.create_from_image(_image, 0)
+	sprite.texture = texture
+
+
+func randomize() -> void:
+	pass
+
+
+
 ## Private Methods
 func _on_mouse_entered():
 	_hovered = true
+	update()
 
 
 func _on_mouse_exited():
 	_hovered = false
+	update()
 
 
 func _on_input_event(viewport : Node, event : InputEvent, shape_idx : int):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and not event.pressed:
 			_selected = not _selected
+			update()
