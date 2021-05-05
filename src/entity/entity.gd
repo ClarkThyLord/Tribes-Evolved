19fd999187210002dbd72a098c085ab065f7a900
ClarkThyLord <ccpotential@gmail.com>
2021-05-04 23:46:53 -0700

    Rework entity
    Clark was here :V

--- src/entity/entity.gd
+++ src/entity/entity.gd
@@ -3,44 +3,37 @@ extends Area2D
 ## Entity
 
 
-## Enums
-enum States {
-	IDLE,
-	SEEKING,
-	MOVING,
-	EATING,
-	ATTACKING,
-	RETREATING,
-	REPRODUCING,
-}
+
+## Signals
+signal eaten(entity)
 
 
 
 ## Exported Variables
+export var color := Color.white setget set_color
+
 export(float, 0.0, 1000.0) var energy := 25.0 setget set_energy
 
 export(float, 0.0, 100.0) var speed := 22.0 setget set_speed
 
 export(float, 16.0, 100.0) var vision := 32.0 setget set_vision
 
-export var variance := Color.white setget set_variance
-
 export(int, 5, 64) var potential := 5 setget set_potential
 
-export var parent_world : NodePath
+export var world : NodePath setget set_world
 
 
 
 ## Private Variables
-var _hovered := false
-
-var _selected := false
-
-var _image : Image = null
+var _world
 
 var _target := Vector2.INF
 
-var _state : int = States.IDLE
+var _image : Image = null
+
+var _hovered := false
+
+var _selected := false
 
 
 
@@ -57,6 +50,11 @@ onready var sprite : Sprite = get_node("Sprite")
 
 ## Built-In Virtual Methods
 func _ready() -> void:
+	if Engine.editor_hint:
+		return
+	
+	self.world = world
+	
 	if is_instance_valid(collision_shape) \
 			and is_instance_valid(collision_shape.shape):
 		collision_shape.shape = collision_shape.shape.duplicate()
@@ -72,8 +70,7 @@ func _process(delta : float) -> void:
 	if Engine.editor_hint:
 		return
 	
-	var world = get_node_or_null(parent_world)
-	if is_instance_valid(world) \
+	if is_instance_valid(_world) \
 			and is_instance_valid(view):
 		var areas := view.get_overlapping_areas()
 		var best_target = [-INF, Vector2.INF]
@@ -85,7 +82,7 @@ func _process(delta : float) -> void:
 			var distance := position.distance_to(area.position)
 			
 			if area.is_in_group("foods"):
-				points = area.nutrition
+				points = area.energy
 				if points > best_target[0]:
 					best_target[0] = points
 					best_target[1] = area.position
@@ -98,7 +95,9 @@ func _process(delta : float) -> void:
 		_target = best_target[1]
 	
 	if _target == Vector2.INF:
-		_target = world.random_world_point()
+		_target = position + Vector2(
+			(randi() % int(vision * 2)) - vision,
+			(randi() % int(vision * 2)) - vision)
 	
 	var distance := position.distance_to(_target)
 	if distance > 8:
@@ -125,7 +124,7 @@ func _draw() -> void:
 			draw_line(
 				to_local(position),
 				to_local(_target),
-				variance,
+				color,
 				2
 			)
 			
@@ -136,7 +135,7 @@ func _draw() -> void:
 				deg2rad(target_angle - 32),
 				deg2rad(target_angle + 32),
 				3,
-				variance,
+				color,
 				6
 			)
 		
@@ -146,19 +145,51 @@ func _draw() -> void:
 			0,
 			deg2rad(360),
 			16,
-			variance,
+			color,
 			2
 		)
 
 
 
 ## Public Methods
+func randomize(parent_a = null, parent_b = null) -> void:
+	if is_instance_valid(parent_a) and is_instance_valid(parent_b):
+		pass
+	else:
+		self.color = Color(randi())
+		self.energy = 25 + (100 * (1 - color.gray()))
+		self.speed = 14 + (16 * color.r)
+		self.vision = 30 + (16 * color.b)
+		self.potential = 5 + int(3 * color.g)
+		
+		_image = Image.new()
+		_image.create(potential, potential, false, Image.FORMAT_RGB8)
+		_image.lock()
+		var axis = (potential - 1) / 2
+		for x in range(axis + 1):
+			for y in range(potential):
+				if (randi() % 10) % 2 == 0:
+					_image.set_pixel(x, y, color)
+					if not x == axis:
+						_image.set_pixel(potential - x - 1, y, color)
+		_image.unlock()
+		
+		var texture = ImageTexture.new()
+		texture.create_from_image(_image, 0)
+		get_node("Sprite").texture = texture
+
+
+func set_world(node_path : NodePath) -> void:
+	world = node_path
+	if not world.is_empty():
+		_world = get_node_or_null(world)
+
+
 func set_energy(value : float) -> void:
 	energy = value if value > 0.0 else 0.0
 	
 	scale = Vector2.ONE * max(clamp(energy / 200.0, 1.0, 3.0), scale.x)
-	if energy <= 0.0:
-		print("die")
+	property_list_changed_notify()
 
 
 func set_speed(value : float) -> void:
@@ -171,85 +202,52 @@ func set_vision(value : float) -> void:
 	if is_instance_valid(view_shape) \
 			and is_instance_valid(view_shape.shape):
 		(view_shape.shape as CircleShape2D).radius = vision
+	property_list_changed_notify()
 
 
-func set_variance(color : Color) -> void:
-	variance = color
+func set_color(value : Color) -> void:
+	color = value
 
 
 func set_potential(value : int) -> void:
 	potential = value + ((value + 1) % 2)
-	property_list_changed_notify()
 	
 	if is_instance_valid(collision_shape) \
 			and is_instance_valid(collision_shape.shape):
 		(collision_shape.shape as RectangleShape2D).extents = Vector2.ONE * potential
+	property_list_changed_notify()
 
 
-func randomize() -> void:
-	self.variance = Color(randi())
-	self.speed = 14 + (16 * variance.r)
-	self.vision = 30 + (16 * variance.b)
-	self.potential = 5 + int(3 * variance.g)
-	birth()
+func get_dominant_color() -> int:
+	var dominant_color := 0 if color[0] > color[1] else 1
+	dominant_color = dominant_color if color[dominant_color] > color[2] else 2
+	return dominant_color
 
 
 func select() -> void:
 	_selected = true
 	
-	var world := get_node_or_null(parent_world)
-	if is_instance_valid(world):
-		world.get_player().select(self)
+	if is_instance_valid(_world):
+		_world.get_player().select(self)
 
 
 func unselect() -> void:
 	_selected = false
 	
-	var world := get_node_or_null(parent_world)
-	if is_instance_valid(world):
-		world.get_player().unselect(self)
+	if is_instance_valid(_world):
+		_world.get_player().unselect(self)
 
 
-func birth() -> void:
-	_image = Image.new()
-	_image.create(potential, potential, false, Image.FORMAT_RGB8)
-	evolve()
+func eat(consumable) -> void:
+	self.energy += consumable.eaten(self)
 
 
-func mutate() -> void:
-	pass
-
-
-func evolve() -> void:
-	if not is_instance_valid(_image):
-		printerr("need to call on entity's birth first!")
-		return
-	
-	var axis = (potential - 1) / 2
-	
-	_image.lock()
-	for x in range(axis + 1):
-		for y in range(potential):
-			if (randi() % 10) % 2 == 0:
-				_image.set_pixel(x, y, variance)
-				if not x == axis:
-					_image.set_pixel(potential - x - 1, y, variance)
-	_image.unlock()
-	
-	var texture = ImageTexture.new()
-	texture.create_from_image(_image, 0)
-	get_node("Sprite").texture = texture
-
-
-func eat(nutrition) -> void:
-	self.energy += nutrition
-
-
-func eaten(eater) -> void:
-	var dominant_color := 0 if eater.variance[0] > eater.variance[1] else 1
-	dominant_color = dominant_color if eater.variance[dominant_color] > eater.variance[2] else 2
-	eater.eat(energy * variance[dominant_color])
+func eaten(by) -> float:
 	queue_free()
+	emit_signal("eaten", self)
+	
+	return energy * color[by.get_dominant_color()]
+
 
 
 ## Private Methods
@@ -272,8 +270,5 @@ func _on_input_event(viewport : Node, event : InputEvent, shape_idx : int) -> vo
 
 
 func _on_area_entered(area : Area2D) -> void:
-	if area.is_in_group("foods"):
-		area.eaten(self)
-	elif area.is_in_group("entities"):
-		if not variance.is_equal_approx(area.variance) and area.energy < energy:
-			area.eaten(self)
+	if area.is_in_group("consumables"):
+		eat(area)
